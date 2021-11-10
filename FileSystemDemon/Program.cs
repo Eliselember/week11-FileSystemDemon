ea65d47ef480eebc8846176acf7bc9e107f9f158
Opilane TTHK <opilane@tthk.ee>
2021-11-10 10:41:50 +0200

    week11

---
+++ FileSystemDemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSystemDemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
